dfa31828f7f9f3fad486aaebf4a2ff2855c115b9
Peter Broman <jp.broman@telia.com>
2026-06-08 14:04:50 +0200

    Base skeleton for my StorageAPI

---
+++ StorageApi.API/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace StorageApi.API.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ StorageApi.TEST/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace StorageApi.TEST;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
